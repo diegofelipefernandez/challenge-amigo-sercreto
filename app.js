5b0c7ed1743479e9dbb70283194b2361d4a657ec
Diego Fernandez <menesesdiegofelipe18@gmail.com>
2025-03-13 18:53:06 -0500

    Actualiza el app: Crea la función agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let amigo = document.getElementById('amigo').value;
+    
+    if (amigo !== '') {
+        amigos.push(amigo);
+        alert('Amigo agregado.');
+        document.getElementById('amigo').value = '';
+    } else {
+        alert('Por favor, inserte un nombre.');
+    }
+}
+
