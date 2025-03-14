94ae8721f38e7dea23396cb2de57e0729f181496
Diego Fernandez <menesesdiegofelipe18@gmail.com>
2025-03-13 19:24:43 -0500

    Actualiza el app: Crea la función amigoAleatorio

--- app.js
+++ app.js
@@ -26,3 +26,21 @@ function listaAmigos() {
         item.appendChild(document.createTextNode(amigos[i]));
     }
 }
+
+/*
+    Escribe una función que seleccione de manera aleatoria uno de los nombres almacenados
+    en el array amigos. Usa Math.random() y Math.floor() para obtener un índice aleatorio.
+*/
+function amigoAleatorio() {
+    // Comprobamos si el arreglo está vacío antes de sortear
+    if (amigos.length === 0) {
+        alert('No hay amigos en la lista.');
+    } else {
+        // Usamos Math.random() y Math.floor() para obtener un índice aleatorio del arreglo
+        let indice = Math.floor(Math.random() * amigos.length);
+        // Utilizamos el índice para acceder al nombre correspondiente
+        let nombre = amigos[indice];
+        // Usamos document.getElementById() e innerHTML para mostrar el amigo soretaeado
+        document.getElementById('resultado').innerHTML = nombre;
+    }
+}
