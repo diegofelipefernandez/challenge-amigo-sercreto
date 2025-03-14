3d5a8ba4c72a227db9636ad1178047e3ed7588ff
Diego Fernandez <menesesdiegofelipe18@gmail.com>
2025-03-14 08:54:45 -0500

    Update app.js: Corregimos la función visualizar los nombres

--- app.js
+++ app.js
@@ -5,25 +5,26 @@ function agregarAmigo() {
     
     if (amigo !== '') {
         amigos.push(amigo);
-        alert('Amigo agregado.');
         document.getElementById('amigo').value = '';
+        actualizarListaAmigos(); // Llamamos a la función para actualizar la lista en pantalla
     } else {
         alert('Por favor, inserte un nombre.');
     }
 }
 
-// Creamos una función para recorrer el arreglo de amigos y mostrarlos en una lista
-function listaAmigos() {
-    // Obtenemos el elemento de la lista
-    let lista = document.getElementById('listaAmigos');
-    // Limpiamos la lista existente
-    lista.innerHTML = '';
-    // Iteramos sobre el arreglo
-    for (let i=0; i<amigos.length; i++) {
-        // Creamos un elemento de lista
-        let item = document.createElement('li');
-        // Agregamos elementos a la lista, creando un nuevo elemento de lista
-        item.appendChild(document.createTextNode(amigos[i]));
+/*
+    Crear una función que recorra el array amigos y agregar cada nombre con un elemento <li>
+    dentro de una lista <ul> en el documento HTML. Utilizano innerHTML para limpiar la lista
+    antes de agregar nuevos elementos.
+*/
+function actualizarListaAmigos() {
+    let lista = document.getElementById('listaAmigos'); // Seleccionamos la lista
+    lista.innerHTML = ''; // Limpiamos la lista antes de agregar nuevos elementos
+
+    for (let amigo of amigos) {
+        let li = document.createElement('li'); // Creamos un nuevo elemento <li>
+        li.textContent = amigo; // Asignamos el nombre del amigo al elemento <li>
+        lista.appendChild(li); // Agregamos el elemento <li> a la lista <ul>
     }
 }
 
