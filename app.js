b10e774c4f744927dd66f945ce2303c75ceb26e1
Diego Fernandez <menesesdiegofelipe18@gmail.com>
2025-03-13 19:02:36 -0500

    Actualiza el app: Crea la función listaAmigos

--- app.js
+++ app.js
@@ -12,3 +12,17 @@ function agregarAmigo() {
     }
 }
 
+// Creamos una función para recorrer el arreglo de amigos y mostrarlos en una lista
+function listaAmigos() {
+    // Obtenemos el elemento de la lista
+    let lista = document.getElementById('listaAmigos');
+    // Limpiamos la lista existente
+    lista.innerHTML = '';
+    // Iteramos sobre el arreglo
+    for (let i=0; i<amigos.length; i++) {
+        // Creamos un elemento de lista
+        let item = document.createElement('li');
+        // Agregamos elementos a la lista, creando un nuevo elemento de lista
+        item.appendChild(document.createTextNode(amigos[i]));
+    }
+}
